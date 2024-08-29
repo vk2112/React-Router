4560a63279cca02024102f3b999cef452fcb50f4
vk2112 <vijaykumar56.vk54@gmail.com>
2024-08-29 13:14:28 +0530

    React-router

---
+++ reactRouter/src/components/Github/Github.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react'
+import { useLoaderData } from 'react-router-dom'
+
+function Github() {
+    const data = useLoaderData();
+    // const [data, setData] = useState([
+    // ])
+    // useEffect(() => {
+    //     fetch('https://api.github.com/users/vk2112')
+    //     .then(res => res.json())
+    //     .then(data => {
+    //         console.log(data);
+    //         setData(data)
+            
+    //     })
+    // },[])
+  return (
+    <div className='bg-gray-600 text-center m-4 text-white p-4'>Github followers: {data.followers}
+    <img src={data.avatar_url} alt="Git-picture" width={300}/>
+    </div>
+  )
+}
+
+export default Github
+
+export const githubInfoLoader = async () => {
+    const response = await fetch('https://api.github.com/users/vk2112')
+    return response.json()
+}
